182ad3482249b3d3d25bd6d6653846eef1f5e934
Rohan Mishra <rohanmishra.004@gmail.com>
2026-01-02 13:21:50 +0530

    channels implementation

--- GoRoutines/main.go
+++ GoRoutines/main.go
@@ -14,18 +14,28 @@ func main() {
 		"http://amazon.com",
 		"http://facebook.com",
 	}
+	//Inside the main func we will create our first channel to interact with different go routines
+
+	//syntax for creating channel
+	c := make(chan string)
+
 	//create a new thread for go routine
 	for _, link := range links {
-		go checkLink(link)
+		//since we have created the channel we need to pass it to the function
+		go checkLink(link, c)
 	}
+	fmt.Println(<-c)
 }
 
 //We can make use of go routines to make these calls run in parallel
 
-func checkLink(link string) {
+// we need to update the function as well here
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, " Might be down")
+		c <- "might be down"
 	}
 	fmt.Println(link, " is up!!!")
+	c <- "link is up"
 }
